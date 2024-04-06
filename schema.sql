be22e95ae73ad814c496a0693009ec6e939edf7c
aatls <aatos.latva-somppi@helsinki.fi>
2024-04-07 00:55:31 +0300

    added sql schema

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE videos (
+    id SERIAL PRIMARY KEY,
+    audioaddress TEXT,
+    videoaddress TEXT,
+    title TEXT,
+    description TEXT,
+    viewcount INT,
+    submissiontime TIMESTAMP
+);
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    submissiontime TIMESTAMP
+);
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    videoid INT,
+    content TEXT,
+    submissiontime TIMESTAMP
+);
