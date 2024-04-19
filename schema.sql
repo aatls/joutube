d007b04a8b3f7bce0b15aaabce38422ad8228153
aatls <aatos.latva-somppi@helsinki.fi>
2024-04-20 01:16:36 +0300

    add users to schema

--- schema.sql
+++ schema.sql
@@ -1,25 +1,32 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT,
+    username TEXT UNIQUE,
     password TEXT
 );
 CREATE TABLE videos (
     id SERIAL PRIMARY KEY,
     audioaddress TEXT,
     videoaddress TEXT,
+    userid INT,
     title TEXT,
     description TEXT,
     viewcount INT,
-    submissiontime TIMESTAMP
+    submissiontime TIMESTAMP,
+    FORGEIN KEY (userid) REFERENCES users(id)
 );
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
-    submissiontime TIMESTAMP
+    submissiontime TIMESTAMP,
+    userid INT,
+    FORGEIN KEY (userid) REFERENCES users(id)
 );
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     videoid INT,
+    userid INT,
     content TEXT,
-    submissiontime TIMESTAMP
+    submissiontime TIMESTAMP,
+    FORGEIN KEY (videooi) REFERENCES videos(id),
+    FORGEIN KEY (userid) REFERENCES users(id)
 );
