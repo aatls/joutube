fbd0e2236ba674fbfb52c16623ee77e828ca8540
aatls <aatos.latva-somppi@helsinki.fi>
2024-04-23 13:05:16 +0300

    add example data

--- schema.sql
+++ schema.sql
@@ -12,14 +12,14 @@ CREATE TABLE videos (
     description TEXT,
     viewcount INT,
     submissiontime TIMESTAMP,
-    FORGEIN KEY (userid) REFERENCES users(id)
+    FOREIGN KEY (userid) REFERENCES users(id)
 );
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     submissiontime TIMESTAMP,
     userid INT,
-    FORGEIN KEY (userid) REFERENCES users(id)
+    FOREIGN KEY (userid) REFERENCES users(id)
 );
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
@@ -27,6 +27,68 @@ CREATE TABLE comments (
     userid INT,
     content TEXT,
     submissiontime TIMESTAMP,
-    FORGEIN KEY (videooi) REFERENCES videos(id),
-    FORGEIN KEY (userid) REFERENCES users(id)
+    FOREIGN KEY (videoid) REFERENCES videos(id),
+    FOREIGN KEY (userid) REFERENCES users(id)
+);
+
+-- Example data
+
+INSERT INTO users (username, password)
+VALUES ('Herra', 'Password');
+
+INSERT INTO videos(
+    audioaddress,
+    videoaddress,
+    userid,
+    title,
+    description,
+    viewcount,
+    submissiontime
+)
+VALUES (
+    'ZeBKgxqfvN0',
+    'nwfCoKNI5hs',
+    (SELECT id FROM users WHERE username='Herra'),
+    'My Foggy Valentine Breakdown',
+    'bojojoing',
+    0,
+    NOW()
+);
+
+INSERT INTO videos(
+    audioaddress,
+    videoaddress,
+    userid,
+    title,
+    description,
+    viewcount,
+    submissiontime
+)
+VALUES (
+    'LB9lObWclFQ',
+    'fdGWRq1dVBA',
+    (SELECT id FROM users WHERE username='Herra'),
+    'Ka-chow',
+    'This is a video description',
+    0,
+    NOW()
+);
+
+INSERT INTO videos(
+    audioaddress,
+    videoaddress,
+    userid,
+    title,
+    description,
+    viewcount,
+    submissiontime
+)
+VALUES (
+    'upbwQPeGm0Q',
+    '6Maq5IyHSuc',
+    (SELECT id FROM users WHERE username='Herra'),
+    'Educational',
+    'Nice components!',
+    0,
+    NOW()
 );
